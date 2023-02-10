956c9ed93e0ea9b485979448d51bdb1d87666919
Ravikar Sahay <ravikarsahay55@gmail.com>
2023-02-10 22:17:59 +0530

    latest push todo

--- context/constants.js
+++ context/constants.js
@@ -1,4 +1,4 @@
 import TodolistJson from './TodoListDApp.json';
 
-export const todoListContractAddress = "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9";
+export const todoListContractAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
 export const todoListAbi = TodolistJson.abi;
